3cf6e1ee21781613c69f5a51320e48289f01a830
Jonathan Boilesen <jonathan@boilesen.com>
2013-10-03 19:11:42 -0300

    Estrutura Inicial do Projeto

---
+++ MC857/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<link type="text/css" href="css/style.css" rel="stylesheet" />
+		<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+	<body>
+	</body>
+</html>
