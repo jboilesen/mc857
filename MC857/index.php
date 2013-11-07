785af146f546450b79737ed1755fb18f21e32391
thiagpastore <thiagpastore@gmail.com>
2013-11-06 22:49:12 -0200

    Eliminando matérias já cursadas via jQuery, através do xml passado por JSON.

--- MC857/index.php
+++ MC857/index.php
@@ -1,21 +1,55 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<link type="text/css" href="css/style.css" rel="stylesheet" />
-		<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
-		<script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
-		<script type="text/javascript" src="js/script.js"></script>
-	</head>
-	<body>
-		<h1>Seja Bem-Vindo ao Sistema de Integraliza&ccedil;&atilde;o!</h1>
-		
-		<fieldset>
-			<legend>Integraliza&ccedil;&atilde;o Curricular</legend>
-			<form name="integralizacao" id="form_integralizacao" action="control/Integralizacao.php" method="post" >
-				<input type="text" name="ra" id="ra" maxlength="6" />
-				<input type="submit" name="submit" value="Gerar Integraliza&ccedil;&atilde;o" />
-			</form>
-		</fieldset>
-		<div id="integralizacao"></div>
-	</body>
+    <head>
+        <link type="text/css" href="css/style.css" rel="stylesheet" />
+        <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+        <h1>Seja Bem-Vindo ao Sistema de Integraliza&ccedil;&atilde;o!</h1>
+
+        
+            <form name="integralizacao" id="form_integralizacao" action="control/Integralizacao.php" method="post" >
+                <fieldset>
+            <legend>Integraliza&ccedil;&atilde;o Curricular</legend>
+                <input type="text" name="ra" id="ra" maxlength="6" />
+                <input type="submit" name="submit" value="Gerar Integraliza&ccedil;&atilde;o" />
+                </fieldset>
+                <div id="div_aluno" style="margin-top: 10px;">
+                    <table>
+                        <tr>
+                            <td>
+                                <label>RA</label>
+                            </td>
+                            <td>
+                                <label >Nome</label>
+                            </td>
+                            <td>
+                                <label>Curso</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input id="input_ra" style="width: 80px" disabled="true"/>
+                            </td>
+                            <td>
+                                <input id="input_nome" style="width: 250px" disabled="true"/>
+                            </td>
+                            <td>
+                                <input id="input_curso" style="width: 300px" disabled="true"/>
+                            </td>
+                        </tr>
+                    </table>
+                    <br/>
+                    <label>N&uacute;cleo Comum ao Curso:</label>
+                    <br/>
+                    <!-- text-decoration: line-through-->
+                </div>
+            </form>
+        
+        <div id="integralizacao">
+        </div>
+
+    </body>
 </html>
--- MC857/js/script.js
+++ MC857/js/script.js
@@ -1,10 +1,86 @@
-$(document).on("submit", 'form', function(event){
-	event.preventDefault();
-	$.post($(this).attr("action"),
-			{name: $(this).attr("name"), form:$(this).serializeArray()},
-			function(value){
-				$("#integralizacao").html(value);
-			}
-		);
-	return false;
+$(document).on("submit", 'form', function(event) {
+    event.preventDefault();
+    $.post($(this).attr("action"),
+            {name: $(this).attr("name"), form: $(this).serializeArray()},
+    function(value) {
+        $("#integralizacao").html(value);
+        var integraliz = JSON.parse(value);
+        //$("#integralizacao").empty();
+        $("#id_table_nucleo_comum").empty();
+        //$("#div_aluno").css('display', 'anything');
+        $("#input_ra").val(integraliz.aluno.ra);
+        $("#input_nome").val(integraliz.aluno.nome);
+        $("#input_curso").val(integraliz.curso.nome);
+        //alert(integraliz.curso["obrigatorias"][0]);
+        montar_obrigatoria(integraliz.curso["obrigatorias"], integraliz.historico);
+        //montar_eletiva(integraliz.curso["modalidades"]["obrigatorias"], integraliz.historico);
+        $('<label></label>').html("Modalidade").appendTo("div_aluno");
+        $('<br/>');
+    }
+    );
+    return true;
 });
+
+
+function montar_obrigatoria(jsCatalogo, jsHistorico) {
+    $('<table></table>').attr({id: "id_table_nucleo_comum"}).appendTo("#div_aluno");
+    var ii = j = 0;
+    for (i = 0; i < jsCatalogo.length; i++) {
+        if (ii <= 0) {
+            $('<tr></tr>').attr({id: "id_tr" + j}).appendTo("#id_table_nucleo_comum");
+        }
+        if (jsHistorico.hasOwnProperty(jsCatalogo[i])) {
+            $('<td></td>').appendTo("#id_tr" + j).html(jsCatalogo[i]).css('color','red');
+        }
+        else {
+            $('<td></td>').appendTo("#id_tr" + j).html(jsCatalogo[i]);
+        }
+
+        ii++;
+        if (ii > 4) {
+            ii = 0;
+            j++;
+        }
+    }
+}
+
+
+function montar_eletiva(jsCatalogo, jsHistoricoMod) {
+    $('<table></table>').attr({id: "id_table_nucleo_modalidade"}).appendTo("#div_aluno");
+    var ii = j = 0;
+    for (i = 0; i < jsCatalogo.length; i++) {
+        if (ii <= 0) {
+            $('<tr></tr>').attr({id: "id_tr_mod" + j}).appendTo("#id_table_nucleo_modalidade");
+        }
+        if (jsHistoricoMod.hasOwnProperty(jsCatalogo[i])) {
+            $('<td></td>').appendTo("#id_tr_mod" + j).html(jsCatalogo[i]).css('color','red');
+        }
+        else {
+            $('<td></td>').appendTo("#id_tr_mod" + j).html(jsCatalogo[i]);
+        }
+        ii++;
+        if (ii > 4) {
+            ii = 0;
+            j++;
+        }
+    }
+}
+
+function montar_tabela(jsHistorico) {
+    $('<table></table>').attr({id: "id_table_nucleo_comum"}).appendTo("#div_aluno");
+    var i = j = 0;
+    $.each(jsHistorico, function(sigla) {
+        alert(sigla);
+        if (i <= 0) {
+            $('<tr></tr>').attr({id: "id_tr" + j}).appendTo("#id_table_nucleo_comum");
+        }
+        $('<td></td>').appendTo("#id_tr" + j).html(sigla);
+        i++;
+        if (i > 4) {
+            i = 0;
+            j++;
+        }
+    }
+    );
+
+}
