b35d133cee64b629f734c9da5aa01184c52da3e6
thiagpastore <thiagpastore@gmail.com>
2013-11-21 16:50:45 -0200

    Arrumei um detalhe busca exaustiva

--- MC857/js/script.js
+++ MC857/js/script.js
@@ -14,7 +14,12 @@ $(document).on("submit", 'form', function(event) {
         $("#input_ra").val(integraliz.aluno.ra);
         $("#input_nome").val(integraliz.aluno.nome);
         $("#input_curso").val(integraliz.curso.nome);
+        //var start = new Date().getMilliseconds();
         montar_integralizacao(integraliz);
+        /*var stop = new Date().getMilliseconds();
+        var executionTime = stop - start;
+
+        alert("Tempo de execucao:" + executionTime + " milisegundos");*/
     }
     );
     return true;
@@ -24,14 +29,13 @@ $(document).on("submit", 'form', function(event) {
 function montar_grupos_eletiva(catalogo, grupo_eletivas) {
     if (catalogo.hasOwnProperty("eletivas")) {
         var eletivas_comum = catalogo.eletivas;
-        for (k = 0; k < eletivas_comum.length; k++) {
+        for (var k = 0; k < eletivas_comum.length; k++) {
             eletivas_comum[k];
             var eletivas = {"cred": 0,
-                            "feitos": 0,
-                            "disciplinas": [],
-                            "alocacao": [],
-                            "fim": 0,
-                            "alocacao_final": []};
+                "feitos": 0,
+                "disciplinas": [],
+                "alocacao": [],
+                "alocacao_final": []};
             eletivas.cred = eletivas_comum[k]["cred"];
             eletivas.disciplinas = eletivas_comum[k].disciplinas;
             var tamanho = grupo_eletivas.eletivas.length;
@@ -41,8 +45,8 @@ function montar_grupos_eletiva(catalogo, grupo_eletivas) {
     if (catalogo.hasOwnProperty("modalidades")) {
         if (catalogo.modalidades[0].hasOwnProperty("eletivas")) {
             var eletivas_mod = catalogo.modalidades[0].eletivas;
-            for (k = 0; k < eletivas_mod.length; k++) {
-                var eletivas = {"cred": 0, "feitos": 0, "disciplinas": [], "alocacao": [], "fim": 0, "alocacao_final": []};
+            for (var k = 0; k < eletivas_mod.length; k++) {
+                var eletivas = {"cred": 0, "feitos": 0, "disciplinas": [], "alocacao": [], "alocacao_final": []};
                 eletivas.cred = parseInt(eletivas_mod[k]["cred"]);
                 eletivas.disciplinas = eletivas_mod[k].disciplinas;
                 var tamanho = grupo_eletivas.eletivas.length;
@@ -53,33 +57,10 @@ function montar_grupos_eletiva(catalogo, grupo_eletivas) {
 }
 
 
-//ordena grupo_eletivas pelo numero de disciplinas e segundo pelo numero de creditos faltantes
-function sort_numero_elementos(a, b) {
-    var a_tamanho = a.disciplinas.length;
-    var b_tamanho = b.disciplinas.length;
-    var pular = false;
-    if (a_tamanho === 1 & a.disciplinas[0] === '-----') {
-        a_tamanho = Number.POSITIVE_INFINITY;
-        pular = true;
-    }
-    if (b_tamanho === 1 & b.disciplinas[0] === '-----') {
-        b_tamanho = Number.POSITIVE_INFINITY;
-        pular = true;
-    }
-    if (!pular) {
-        if (a_tamanho === b_tamanho) {
-            a_tamanho = a.cred - a.feitos;
-            b_tamanho = b.cred - b.feitos;
-
-        }
-    }
-    return ((a_tamanho < b_tamanho) ? -1 : ((a_tamanho > b_tamanho) ? 1 : 0));
-}
-
-
 json_grupo_eletivas = {eletivas: []};
 historico;
 function montar_integralizacao(integralizacao) {
+
     $("#div_aluno").append('<div id="div_nucleo_comum" style="margin-top:10px; margin-left:10px;" ></div>');
     montar_obrigatoria(integralizacao.curso["obrigatorias"], integralizacao.historico, "#div_nucleo_comum");
     if (integralizacao.curso.hasOwnProperty("modalidades")) {
@@ -92,16 +73,16 @@ function montar_integralizacao(integralizacao) {
     json_grupo_eletivas = {eletivas: []};
     montar_grupos_eletiva(integralizacao.curso, json_grupo_eletivas);
 
-    json_grupo_eletivas.eletivas.sort(sort_numero_elementos);
     historico = integralizacao.historico;
-    
+
     var grupo_eletivas = json_grupo_eletivas.eletivas;
     tamanho_hist = tamanho_historico(historico);
 
-    //contador = 0;
+    contador = 0;
+    $("div_aluno").append('.');
     var t = tenta(0, grupo_eletivas);
-    //alert(t + '   ' +contador);
-    
+
+
     $("#div_aluno").append('<div id="div_eletivas" style="margin-top:10px; margin-left:10px;" ></div>');
     exibir_eletivas_restantes(historico, grupo_eletivas, "#div_eletivas");
 
@@ -122,6 +103,8 @@ function montar_integralizacao(integralizacao) {
         $('#id_label_disciplinas_rest').html("Integraliza&ccedil;&atilde;o Completa! Aluno(a) cumpriu todas mat&eacute;rias do curr&iacute;culo.");
     }
     imprime_extra_curriculares(integralizacao.historico, '#div_aluno');
+
+
 }
 
 // Eliminar do historico as disciplinas obrigatorias jah cursadas
@@ -166,7 +149,7 @@ function montar_modalidade(jsCatalogo, jsHistorico, id_div_pai) {
     var ii = -1;
     var j = 0;
     var creditos_feitos = 0;
-    for (i = 0; i < jsCatalogo.length; i++) {
+    for (var i = 0; i < jsCatalogo.length; i++) {
         if (ii < 0) {
             $('<tr></tr>').attr({id: "id_tr_mod" + j}).appendTo("#id_table_modalidade");
             ii++;
@@ -196,19 +179,18 @@ function montar_modalidade(jsCatalogo, jsHistorico, id_div_pai) {
 // completa todos os grupos de eletivas 
 function contem_solucao(grupo_eletivas) {
     var grupos_finalizados = 0;
-    var cred_total = 0;
     var feitos_total = 0;
-    for (i = 0; i < grupo_eletivas.length; i++) {
+    for (var i = 0; i < grupo_eletivas.length; i++) {
         var cred = grupo_eletivas[i].cred;
         var feitos = grupo_eletivas[i].feitos;
-        cred_total += cred;
         feitos_total += feitos;
-        if (cred - feitos <= 0) {
+        if (cred <= feitos) {
             grupos_finalizados++;
         }
     }
     if (grupos_finalizados > 0) {
         if (grupos_finalizados === grupo_eletivas.length) {
+
             return true;
         }
     }
@@ -226,15 +208,18 @@ function tamanho_historico(historico) {
 
 // verifica se uma disciplina pertence a algum grupo de eletivas
 function pertence_grupo(disciplina, grupo) {
-    if ((grupo.cred <= grupo.feitos) | grupo.fim === 1) {
+    if ((grupo.cred <= grupo.feitos)) {
         return false;
     }
     if ($.inArray(disciplina, grupo.disciplinas) >= 0) {
         return true;
     } else {
-        for (k = 0; k < grupo.disciplinas.length; k++) {
+        for (var k = 0; k < grupo.disciplinas.length; k++) {
             var eletiva = grupo.disciplinas[k];
-            if (eletiva.indexOf('-') >= 0) {
+            if (eletiva.indexOf('-----') === 0) {
+                return true;
+            }
+            if (eletiva.indexOf('-') > 0) {
                 var eletiva_ = eletiva.replace(/-/g, "");
                 if (disciplina.indexOf(eletiva_) === 0) {
                     return true;
@@ -249,7 +234,7 @@ function pertence_grupo(disciplina, grupo) {
 function get_disciplina_hist(hist, i) {
     var cont = 0;
     var disciplina = {"cod": '', "cred": 0};
-    
+
     $.each(hist, function(d) {
         if (cont === i) {
             disciplina.cod = d;
@@ -275,7 +260,7 @@ function dissociar_grupo_disciplina(grupo, disciplina) {
 // copia a solucao otima ou a melhor encontrada para alocacao
 // de disciplinas nos grupos de eletivas
 function copiar_solucao(c_grupo_eletivas) {
-    for (i = 0; i < c_grupo_eletivas.length; i++) {
+    for (var i = 0; i < c_grupo_eletivas.length; i++) {
         c_grupo_eletivas[i].alocacao_final = c_grupo_eletivas[i].alocacao.slice();
     }
 }
@@ -283,21 +268,22 @@ function copiar_solucao(c_grupo_eletivas) {
 // Faz busca exaustiva para tentar alocar as disciplinas do historico nos grupos
 // de eletivas
 function tenta(i, t_grupo_eletivas) {
+    $("div_aluno").append('.');
     //contador++;
     if (contem_solucao(t_grupo_eletivas)) {
         copiar_solucao(t_grupo_eletivas);
         return true;
     }
     if (i >= tamanho_hist) {
+        //alert(JSON.stringify(t_grupo_eletivas));
         copiar_solucao(t_grupo_eletivas);
         return false;
     }
     var disciplina = get_disciplina_hist(historico, i);
     var achou = false;
     var cont_associada = 0;
-    
-    for (jj = 0; jj < t_grupo_eletivas.length; jj++) {
-            
+    for (var jj = 0; jj < t_grupo_eletivas.length; jj++) {
+        contador++;
         var t_g_eletiva = t_grupo_eletivas[jj];
         var pertence = pertence_grupo(disciplina.cod, t_g_eletiva);
         if (pertence) {
@@ -310,6 +296,7 @@ function tenta(i, t_grupo_eletivas) {
         }
     }
     if (achou === false & cont_associada === 0) {
+        //alert(disciplina.cod);
         achou = tenta(i + 1, t_grupo_eletivas);
     }
     return achou;
@@ -320,10 +307,10 @@ function tenta(i, t_grupo_eletivas) {
 // se existir um grupo nao finalizado exibi as opcoes do grupo e os creditos que
 
 function exibir_eletivas_restantes(historico, grupo_eletivas, id_div_pai) {
-    for (k = 0; k < grupo_eletivas.length; k++) {
+    for (var k = 0; k < grupo_eletivas.length; k++) {
         var g_eletiva = grupo_eletivas[k];
         var cred_restantes = g_eletiva.cred;
-        
+
         $.each(historico, function(d) {
             if ($.inArray(d, g_eletiva.alocacao_final) >= 0) {
                 cred_restantes -= historico[d];
@@ -339,13 +326,13 @@ function exibir_eletivas_restantes(historico, grupo_eletivas, id_div_pai) {
         });
         var ii = -1;
         var j = 0;
-        
+
         if (cred_restantes > 0) {
-            
+
             $(id_div_pai).append('<div id="div_eletiva_' + k + '" style="margin-top:10px;" ></div>');
             $('<label></label>').css({"font-size": "14px"}).html(cred_restantes + " cr&eacute;ditos em:").appendTo("#div_eletiva_" + k);
             $('<table></table>').attr({id: "id_table_eletiva_" + k}).appendTo("#div_eletiva_" + k);
-            for (i = 0; i < g_eletiva.disciplinas.length; i++) {
+            for (var i = 0; i < g_eletiva.disciplinas.length; i++) {
                 if (ii < 0) {
                     $('<tr></tr>').attr({id: "id_tr_ele_" + k + '_' + j}).appendTo("#id_table_eletiva_" + k);
                     ii++;
